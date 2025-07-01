0e423f4c93145a59365fc8a59682ed70afeaa57c
kanzlerxm <143618182+kanzlermb@users.noreply.github.com>
2025-07-01 20:27:59 -0300

    g

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ let tamanhoSenha = 12;
 
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelector('.parametro-senha_botao');
+const botoes = document.querySelectorAll('.parametro-senha_botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
-
+    //tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
 
 function aumentaTamanho(){
-    tamanhoSenha++;
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
